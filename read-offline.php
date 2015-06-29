3b65a97579962b03c352891a3b9dd5038893a0c1
PerS <PerS@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-29 22:02:09 +0000

    committed with grunt-push-svn
    git-svn-id: https://plugins.svn.wordpress.org/read-offline/trunk@1189883 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- read-offline.php
+++ read-offline.php
@@ -2,38 +2,41 @@
 /*
 Plugin Name: Read Offline
 Plugin URI: http://soderlind.no/archives/2012/10/01/read-offline/
-Description: lorem ipsum dolores est
+Description: Read Offline allows you to download or print posts and pages. You can download the posts as PDF, ePub or mobi
 Author: Per Soderlind
-Version: 0.2.3
+Version: 0.3.0
 Author URI: http://soderlind.no
 Text Domain: read-offline
 Domain Path: /languages
 */
+defined( 'ABSPATH' ) or die();
 
+define( 'READOFFLINE_PATH',   __DIR__);
+define( 'READOFFLINE_URL',   plugin_dir_url( __FILE__ ));
+define( 'READOFFLINE_CACHE', WP_CONTENT_DIR . '/cache/read-offline');
+define( 'READOFFLINE_VERSION', '0.3.0' );
 
 
 if ( version_compare( PHP_VERSION, '5.3.0' ) < 0 ) {
     return add_action( 'admin_notices', 'read_offline_admin_notice_php_version' );
 }
 
-
-
-Read_Offline_Loader::autoload(__DIR__);
+Read_Offline_Loader::autoload( READOFFLINE_PATH . '/inc'); // autoload inc/class.*.php files
 
 if ( is_admin() ) {
  	new Read_Offline_Admin_Settings ();
 }
+
 if (get_option( 'Read_Offline_Admin_Settings' )) {
 	add_action( 'init', function(){
 			//Read_Offline::get_instance();
 			Read_Offline_Parser::get_instance();
 			//Read_Offline_Shortcode::get_instance();
 			Read_Offline_UX::get_instance();
-
-	});
-	add_action( 'widgets_init', function(){
-	     register_widget( 'Read_Offline_Widget' );
-	});
+	}, 1);
+	// add_action( 'widgets_init', function(){
+	//      register_widget( 'Read_Offline_Widget' );
+	// });
 } else {
 	if ( is_admin() ) {
 		return add_action( 'admin_notices', 'read_offline_admin_notice_update_options',99 );
@@ -70,18 +73,23 @@ function read_offline_admin_notice_update_options () {
 class Read_Offline_Loader {
 	private static  $dir = __DIR__;
 
-	public static function autoload($dir = '' ) {
-		if ( ! empty( $dir ) )
+	public static function autoload($dir = '') {
+		if ( ! empty( $dir ) ) {
 			self::$dir = $dir;
-
+		}
 		spl_autoload_register(  __CLASS__ . '::loader'  );
 	}
 
 	private static function loader( $class_name ) {
-		$class_path = trailingslashit(self::$dir) . 'class-' . strtolower( str_replace( '_', '-', $class_name ) ) . '.php';
 
-		if ( file_exists( $class_path ) )
+		$filename = sprintf("class-%s.php",strtolower( str_replace( '_', '-', $class_name ) ));
+
+		$class_path = trailingslashit(self::$dir) . $filename;
+
+		if ( file_exists( $class_path ) ) {
 			require_once $class_path;
+		}
+
 	}
 }
 
